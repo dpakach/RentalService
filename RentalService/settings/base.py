170c3bfae1ab134de43f474e8099d42c7ff3a7d9
dpakach <dpakach@gmail.com>
2018-01-27 14:23:16 +0545

    some changes in templates and css added
    some stylesheets added and teplates changed for better rendering

--- RentalService/settings/base.py
+++ RentalService/settings/base.py
@@ -88,8 +88,12 @@ WSGI_APPLICATION = 'RentalService.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'rentalService',
+        'USER': 'deepak',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
