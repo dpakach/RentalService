998c1013bc834acd254bc2fea56576401b8a865a
dpakach <dpakach@gmail.com>
2018-01-27 12:32:25 +0545

    final changes

--- account/urls.py
+++ account/urls.py
@@ -7,5 +7,5 @@ app_name = "accounts"
 urlpatterns = [
     url(r'signup/$', views.signup, name='signup'),
     url(r'login/$', auth_views.login, {'template_name': 'account/login.html'}, name='login'),
-    url(r'logout/$', auth_views.logout, {'next_page': 'login'}, name='logout'),
+    url(r'logout/$', auth_views.logout, {'next_page': 'accounts/login'}, name='logout'),
 ]
