ec90a9d07f67c86552aa5c3237fbd6fe8c0826c7
AjayKarki <ajaykarki333@gmail.com>
2018-01-30 15:07:38 +0545

    All bugs fixed Email verification working fine

--- account/views.py
+++ account/views.py
@@ -2,10 +2,14 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.sites.shortcuts import get_current_site
 from django.shortcuts import render, redirect
 from django.utils.encoding import force_bytes
+from django.utils.encoding import force_text
 from django.utils.http import urlsafe_base64_encode
+from django.utils.http import urlsafe_base64_decode
+
 from django.template.loader import render_to_string
 from .forms import SignUpForm
 from .tokens import account_activation_token
+from django.contrib.auth.models import User
 
 @login_required
 def home(request):
