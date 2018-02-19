9c885efb1ad224ae17ab132dbffbfa7f142f79c9
dpakach <dpakach@gmail.com>
2018-02-19 15:40:08 +0545

    added statid files dirs

--- RentalService/settings/production.py
+++ RentalService/settings/production.py
@@ -151,6 +151,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR,'static-st')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 LOGIN_REDIRECT_URL = '/'
