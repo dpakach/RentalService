0bcdea11a75564edd5505f0e5e006d74eb14a73b
dpakach <dpakach@gmail.com>
2018-02-19 16:57:43 +0545

    add static root

--- RentalService/settings/production.py
+++ RentalService/settings/production.py
@@ -60,7 +60,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    # 'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'RentalService.urls'
