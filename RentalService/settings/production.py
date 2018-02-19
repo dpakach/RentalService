80c338c5f1f601b3fa03137568fdc2ecbc940937
dpakach <dpakach@gmail.com>
2018-02-19 16:47:04 +0545

    whitenoise setting

--- RentalService/settings/production.py
+++ RentalService/settings/production.py
@@ -152,7 +152,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 LOGIN_REDIRECT_URL = '/'
 
