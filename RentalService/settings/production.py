fad80dd92d70d105d70636a067b036b5dff24229
dpakach <dpakach@gmail.com>
2018-02-19 16:29:40 +0545

    static files dirs changed

--- RentalService/settings/production.py
+++ RentalService/settings/production.py
@@ -150,11 +150,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = []
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 LOGIN_REDIRECT_URL = '/'
-
 
 #For email verification
 EMAIL_USE_TLS = True
