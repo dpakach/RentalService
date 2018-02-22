9a903584a03976e45d24221a11ab6762f9e3c0d8
dpakach <dpakach@gmail.com>
2018-02-22 17:57:12 +0545

    added taggit to requirements:

--- rental/settings/production.py
+++ rental/settings/production.py
@@ -93,6 +93,7 @@ import dj_database_url
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 DATABASES['default']['CONN_MAX_AGE'] = 500
+
 # DATABASES = {
 #     'default': {
 #         'ENGINE': 'django.db.backends.postgresql',
