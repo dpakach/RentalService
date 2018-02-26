976e13aeac024c456a092f3dc76fed005faa963b
dpakach <dpakach@gmail.com>
2018-02-26 17:31:56 +0545

    set debug to false in production settings

--- rental/settings/production.py
+++ rental/settings/production.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-DEBUG = True
+DEBUG = False
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
