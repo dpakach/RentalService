9cf4cc4686171043eac82e522dbdf8ef64dbb4fa
dpakach <dpakach@gmail.com>
2018-03-04 19:38:52 +0545

    production setting debug option changed to true for debugging

--- rental/settings/production.py
+++ rental/settings/production.py
@@ -45,7 +45,7 @@ INSTALLED_APPS = [
 
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
-DEBUG = False
+DEBUG = True
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
