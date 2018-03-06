6fba8e9aa47d094e209b7950c07913fc8e2848cf
dpakach <dpakach@gmail.com>
2018-03-06 13:59:46 +0545

    some model changes

--- rental/settings/local.py
+++ rental/settings/local.py
@@ -40,7 +40,7 @@ DATABASES = {
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('DATABASE_NAME'),
+        'NAME':  os.environ.get('DATABASE_NAME'),
         'USER': os.environ.get('DATABASE_USER'),
         'PASSWORD': os.environ.get('DATABASE_PASSWORD'),
         'HOST': os.environ.get('DATABASE_HOST'),
