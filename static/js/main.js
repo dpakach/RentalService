bb7472f695b1b43f5fd825d9900047ff6131cf56
dpakach <dpakach@gmail.com>
2018-03-06 09:21:08 +0545

    some update in public js files

--- static/js/main.js
+++ static/js/main.js
@@ -1,34 +1,8 @@
-//bling.js
-//
-// const $ = document.querySelector.bind(document);
-// const $$ = document.querySelectorAll.bind(document);
-// 
-// Node.prototype.on = window.on = function (name, fn) {
-//   this.addEventListener(name, fn);
-// }
-// 
-// NodeList.prototype.__proto__ = Array.prototype;
-// 
-// NodeList.prototype.on = NodeList.prototype.addEventListener = function (name, fn) {
-//   this.forEach(function (elem, i) {
-//     elem.on(name, fn);
-//   });
-// }
-// 
-// 
+import { $, $$ } from './modules/bling';
+import autocomplete from './modules/autocomplete.js';
 
-function autoComplete(latInput, lngInput){
-        var input = document.getElementById('id_location')
-        var autocomplete = new google.maps.places.Autocomplete(input);
 
-        const dropdown = new google.map.places.Autocomplete(input);
-
-        dropdown.addListener('place_changed', () => {
-                const place = dropdown.getPlace();
-                latInput.value = place.geometry.location.lat();
-                lngInput.value = place.geometry.location.lng();;
-        });
-}
+autocomplete($('#id_latitude'), $('#id_longitude'));
 
 //if someone hit enter in location field dont submit the form
 
